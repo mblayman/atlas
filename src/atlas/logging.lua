ad79b7edb5331b0ac44dfce26faf3add4b2f0f78
Matt Layman <matthewlayman@gmail.com>
2022-01-29 23:57:41 -0500

    Make the logger event loop aware.

--- src/atlas/logging.lua
+++ src/atlas/logging.lua
@@ -1,3 +1,5 @@
+local luv = require "luv"
+
 local config = require "atlas.config"
 
 -- Get a logger object configured to log to a particular logging namespace.
@@ -6,12 +8,19 @@ local config = require "atlas.config"
 local function get_logger(logger)
 
   -- Log a message to the log file.
+  --
+  -- The log function is context aware and will write synchronously
+  -- if the event loop is not running or operate asynchronously when it is.
   local function log(message)
-    -- TODO: This is not async friendly.
-    -- This probably needs to detect if the event loop has started,
-    -- then use a combo of uv.fs_write, callbacks, and coroutines.
-    config.log_file:write(string.format("%s: %s\n", logger, message))
-    config.log_file:flush()
+    if luv.loop_alive() then
+      -- TODO: This is not async friendly yet.
+      -- Use a combo of uv.fs_write, callbacks, and coroutines.
+      config.log_file:write(string.format("%s: %s\n", logger, message))
+      config.log_file:flush()
+    else
+      config.log_file:write(string.format("%s: %s\n", logger, message))
+      config.log_file:flush()
+    end
   end
 
   return {log = log}
