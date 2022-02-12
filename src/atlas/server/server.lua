08dd122b890c44c00b2e6a2b8682d2d35f083740
Matt Layman <matthewlayman@gmail.com>
2022-02-12 01:46:59 -0500

    Clean up the server setup method.

--- src/atlas/server/server.lua
+++ src/atlas/server/server.lua
@@ -36,33 +36,30 @@ end
 --
 -- This allows stubbing of the server in tests.
 function Server._make_tcp_server(self)
-  local tcp_err
-  self._server, tcp_err = luv.new_tcp()
-  if tcp_err then
+  local err
+  self._server, err = luv.new_tcp()
+  if err then
     logger.log("Failed to create a TCP server")
-    logger.log(tcp_err)
+    logger.log(err)
     return 1
   end
   return 0
 end
 
--- Set up the server to handle requests.
---
--- Return 0 if setup is successful or 1 if it failed.
-function Server.set_up(self, config)
-  local tcp_server_status = self:_make_tcp_server()
-  if tcp_server_status ~= 0 then return tcp_server_status end
-
-  local bind_status, bind_err = self._server:bind(config.host, config.port)
-  if bind_status ~= 0 then
+-- Bind to host and port.
+function Server._bind(self, config)
+  local status, err = self._server:bind(config.host, config.port)
+  if status ~= 0 then
     logger.log("Failed to bind to host and port")
-    logger.log(bind_err)
-    return 1
+    logger.log(err)
+    return status
   end
+  return 0
+end
 
-  logger.log("Listening for requests on http://" .. config.host .. ":" .. config.port)
-
-  local listen_callback = function(listen_callback_err)
+-- Make the listen callback.
+function Server._make_listen_callback(self)
+  return function(listen_callback_err)
     -- TODO: This is a callback so how is this supposed to clean up properly
     -- if there is an error?
     assert(not listen_callback_err, listen_callback_err)
@@ -75,23 +72,31 @@ function Server.set_up(self, config)
     -- which somehow is happening outside of the main coroutine.
     coroutine.wrap(function() on_connection(client) end)()
   end
+end
 
-  local listen_status, listen_err = self._server:listen(
-                                      atlas_config.backlog_connections, listen_callback)
-  if listen_status ~= 0 then
+-- Configure the server to listen for connections.
+function Server._listen(self)
+  local status, err = self._server:listen(atlas_config.backlog_connections,
+                                          self:_make_listen_callback())
+  if status ~= 0 then
     logger.log("Failed to listen to incoming connections")
-    logger.log(listen_err)
-    return 1
+    logger.log(err)
+    return status
   end
+  return 0
+end
 
-  -- Clean up immediately.
-  -- When the main loop is running in the default usage,
-  -- SIGINT doesn't respond unless it happens twice.
-  -- This handler ensures that the clean up happens right away.
-  local signal, signal_err = luv.new_signal()
-  if signal_err then
+-- Set sigint handler.
+--
+-- Clean up immediately.
+-- When the main loop is running in the default usage,
+-- SIGINT doesn't respond unless it happens twice.
+-- This handler ensures that the clean up happens right away.
+function Server._set_sigint(_)
+  local signal, err = luv.new_signal()
+  if err then
     logger.log("Failed to set signal handler")
-    logger.log(signal_err)
+    logger.log(err)
     return 1
   end
 
@@ -100,11 +105,33 @@ function Server.set_up(self, config)
     -- That prevents the logger from working.
     -- logger.log("Shutting down")
     -- TODO: better cleanup? close existing handlers?
-    os.exit(1)
+    os.exit(0)
   end
   luv.signal_start(signal, "sigint", on_sigint)
 
   return 0
+end
+
+-- Set up the server to handle requests.
+--
+-- Return 0 if setup is successful or 1 if it failed.
+function Server.set_up(self, config)
+  local status
+  status = self:_make_tcp_server()
+  if status ~= 0 then return status end
+
+  status = self:_bind(config)
+  if status ~= 0 then return status end
+
+  logger.log("Listening for requests on http://" .. config.host .. ":" .. config.port)
+
+  status = self:_listen()
+  if status ~= 0 then return status end
+
+  status = self:_set_sigint()
+  if status ~= 0 then return status end
+
+  return status
 end
 
 -- Run the uv loop.
